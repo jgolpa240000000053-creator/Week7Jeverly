987851f7032faeebd1f6b75ee86d80ba39b10582
jgolpa240000000053-creator <jgolpa_240000000053@uic.edu.ph>
2026-09-09 02:05:26 +0000

    Installation Done

--- Week7Jeverly.py
+++ Week7Jeverly.py
@@ -5,7 +5,8 @@ import os
 try:
     import xmltodict
     HAS_XML_TODICT = True
+    print("Installation Done")
 except ImportError:
-HAS_XML_TODICT = False
-print("Note: xmltodict not installed, Run: pip install xmltodict")
+    HAS_XML_TODICT = False
+    print("Note: xmltodict not installed, Run: pip install xmltodict")
 

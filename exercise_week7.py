0b5fadff2e1cf13d8a6a81f46f7825d3ae8696d3
jgolpa240000000053-creator <jgolpa_240000000053@uic.edu.ph>
2026-09-09 21:44:11 +0800

    Exercise 3 load

--- exercise_week7.py
+++ exercise_week7.py
@@ -51,3 +51,22 @@ yaml_output = yaml.dumps(router_config, default_flow_style=False)
 
 print(yaml_output)
 print()
+
+
+#-------------------
+#EXERCISE 3: Read JSON from a file
+#-------------------
+
+print("=== EXERCISE 2: Read JSON from a file ===")
+#First, let's save our config as a JSON file
+with open("router_config.json", "w") as f:
+    json.dump(router_config, f, indent=2)
+
+#Now read it back
+#FILL IN THE BLANK: use to read from the file
+with open("router_config.json", "r") as f:
+    loaded_config = json.load(f)
+
+print(f"Loaded hostname: {loaded_config['hostname']}")
+print(f"Number of interfaces: {len(loaded_config['interfaces'])}")
+print()
